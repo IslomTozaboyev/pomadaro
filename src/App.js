7242211b100e0ab11333f69869cd6b744d86fabf
IslomTozaboyev <itozaboyev@gmail.com>
2021-07-27 23:55:18 +0500

    pomadaro

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import React from 'react';
 import ClockWidthHook from './components/ClockWidthHook';
 import Loading from './components/Loading';
 import Pomodaro from './Pomodaro';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className='text-center d-flex justify-content-center align-items-center'>
+    <div className='App'>
+      <div className="container d-flex justify-content-between align-items-center text-white my-4">
+        <h2 className="fw-bold">Pomadaro Clock</h2>
+        <FontAwesomeIcon className="fw-bold fs-4" icon={faBars}/>
+      </div>
       {/* <ClockWidthHook/> */}
       {/* <Loading/> */}
-      <Pomodaro workTime={25} restTime={5}/>
       {/* <Pomodaro workTime={30} restTime={10}/> */}
+    <div className='text-center d-flex justify-content-center align-items-center'>
+        <Pomodaro workTime={25} restTime={5}/>
+    </div>
     </div>
   );
 }
